b1aea7b5637fa44d4b4bc44795955beb2aecf791
Ernesto Neto <ernestoborges1995@gmail.com>
2023-05-14 17:17:23 -0300

    test: netlify.toml

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(cors({
-    origin: 'http://localhost:5173', //front-end server address
+    origin: process.env.ENV_URL, //front-end server address
     credentials: true, // allow use of credentials
 }));
 app.use(cookieParser());
