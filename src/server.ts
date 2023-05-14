fb33dab4a68ff39d02f3322bac41c400e8d02553
Ernesto Neto <ernestoborges1995@gmail.com>
2023-05-14 17:30:04 -0300

    test: removed cors origin restriction

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(cors({
-    origin: process.env.ENV_URL, //front-end server address
+    origin: "*", //front-end server address
     credentials: true, // allow use of credentials
 }));
 app.use(cookieParser());
