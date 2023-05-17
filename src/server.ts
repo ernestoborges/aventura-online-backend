23d2ec8e1907876b26c84ca4e8b3487734b26af9
ErnestoBorges <50407385+ernestoborges@users.noreply.github.com>
2023-05-17 14:17:04 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,10 @@ const app: Express = express();
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-
+app.use(cors({
+  origin: "https://aventuraonline.netlify.app",
+  credentials: true
+}));
 app.use(cookieParser());
 app.use(router);
 
