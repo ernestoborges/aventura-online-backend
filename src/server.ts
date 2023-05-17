de012c2d478151666e3d3a64205be89beb98bd91
ErnestoBorges <50407385+ernestoborges@users.noreply.github.com>
2023-05-17 13:33:06 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,6 @@ const app: Express = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
-app.use(cors());
 app.use(cookieParser());
 app.use(router);
 
