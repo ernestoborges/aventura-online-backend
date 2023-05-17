162392215b8a90bc800636846b833fdcfd59ae68
Ernesto Neto <ernestoborges1995@gmail.com>
2023-05-17 10:45:48 -0300

    style: change to routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -1,19 +1,19 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import { login } from './db/controllers/LoginController';
 import { register } from './db/controllers/RegisterController';
 import { getUserCharacters } from './db/controllers/GetCharacterSheetsController';
 import { authMiddleware } from './db/middlewares/authMiddleware';
 import { createCharacter } from './db/controllers/CreateCharacterSheetController';
-import { uploadImage } from './db/controllers/UploadImageController';
 import { getUserData } from './db/controllers/UserController';
 import { handleRefreshToken } from './db/controllers/RefreshTokenController';
+import { uploadImage } from './db/controllers/UploadImageController';
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
 
-router.get('/refresh-token', handleRefreshToken )
+router.get('/refresh-token', handleRefreshToken );
 
 // protected routes
 router.get('/user', authMiddleware, getUserData);
