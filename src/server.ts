b032f03ea6bf161ebfce647d56f261967306c5d9
Ernesto Neto <ernestoborges1995@gmail.com>
2023-05-18 10:18:38 -0300

    test: cors config inside server.ts again

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response, NextFunction } from "express";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
+import cors from "cors";
 import "./db/connect"
 import router from "./routes";
 
@@ -10,6 +11,11 @@ const app: Express = express();
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(cors({
+  origin: process.env.FRONTEND_BASE_URL,
+  credentials: true
+}));
+
 app.use(cookieParser());
 app.use(router);
 
