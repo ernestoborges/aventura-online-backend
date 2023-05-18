0d7a15ac11ad9cf3250fc5561814f1258d59f624
Ernesto Neto <ernestoborges1995@gmail.com>
2023-05-17 21:27:50 -0300

    test: vercel.json

--- src/server.ts
+++ src/server.ts
@@ -11,20 +11,11 @@ const app: Express = express();
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-// app.use(cors({
-//   origin: process.env.FRONTEND_BASE_URL,
-//   credentials: true
-// }));
-app.options('*', (req: Request, res: Response) => {
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    );
-    res.sendStatus(200);
-  });
+app.use(cors({
+  origin: process.env.FRONTEND_BASE_URL,
+  credentials: true
+}));
+
 app.use(cookieParser());
 app.use(router);
 
