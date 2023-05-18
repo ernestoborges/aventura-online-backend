6671d33a22e5e3e46dc2f4549124c28061f9bf69
Ernesto Neto <ernestoborges1995@gmail.com>
2023-05-17 21:14:01 -0300

    test: app.options

--- src/server.ts
+++ src/server.ts
@@ -15,6 +15,16 @@ app.use(cors({
   origin: process.env.FRONTEND_BASE_URL,
   credentials: true
 }));
+app.options('*', (req: Request, res: Response) => {
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    );
+    res.sendStatus(200);
+  });
 app.use(cookieParser());
 app.use(router);
 
