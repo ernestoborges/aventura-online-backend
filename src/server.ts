b2591cefdafa9f6d0dffe16dd901d5439c9ea320
Ernesto Neto <ernestoborges1995@gmail.com>
2023-05-17 21:05:14 -0300

    header to vercel.json

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,7 @@ const app: Express = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors({
-  origin: "https://aventuraonline.netlify.app",
+  origin: process.env.FRONTEND_BASE_URL,
   credentials: true
 }));
 app.use(cookieParser());
