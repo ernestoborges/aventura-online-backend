1fae039089a130c44cc1d56a17aec7cbeaf14820
Ernesto Neto <ernestoborges1995@gmail.com>
2023-05-18 09:07:54 -0300

    test: /hello route to test same origin

--- src/routes.ts
+++ src/routes.ts
@@ -10,10 +10,11 @@ import { uploadImage } from './db/controllers/UploadImageController';
 
 const router = express.Router();
 
+router.get('/hello', (req, res) => res.status(201).json({hi: "hello, world"}))
 router.post('/register', register);
 router.post('/login', login);
 
-router.get('/refresh-token', handleRefreshToken );
+router.get('/refresh-token', handleRefreshToken);
 
 // protected routes
 router.get('/user', authMiddleware, getUserData);
